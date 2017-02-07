41a9d48f486480cca154cac868e16c90d87f8072
php 18 <php.18@metralha.cec.ime.usp.br>
2017-02-06 22:18:00 -0200

    primeiro commit

---
+++ logica/PrimeiroPrograma.php
@@ -0,0 +1,54 @@
+<?php
+
+//$nome = "Gretchen";
+
+//var_dump((string)$nome);
+//echo "Nome: $nome" . PHP_EOL ;
+
+//echo 'Nome: $nome\n';
+
+//EX1
+
+/*for ($i=0; $i < 100; $i++) {
+  echo "vinicius" . PHP_EOL;
+}
+
+//EX2
+for ($i=1; $i <= 100; $i++) {
+  echo "$i" . PHP_EOL;
+}
+
+//EX3
+for ($i=1; $i <= 100; $i++) {
+  if ($i%2 == "true") {
+    echo "$i **,";
+  } else{
+    echo "$i *,";
+  }
+}
+
+//EX4
+for ($i=1; $i <= 100; $i++) {
+  if ($i%4 == "true") {
+    echo "PIN ,";
+  } else{
+    echo "$i ,";
+  }
+}
+
+//EX7
+
+$v1 = 0;
+$v2 = 1;
+for ($i=0; $i < 5; $i++) {
+  echo "$v1";
+}
+
+*/
+EX5
+
+$v1 = "";
+for ($i=0; $i < 5; $i++) {
+  echo "$v1";
+  $v1 .= "*"
+}
