177169e1b862b8cdae070271c58d0a7b16277631
php 18 <php.18@metralha.cec.ime.usp.br>
2017-02-08 22:42:11 -0200

    Arrays

---
+++ arrays/TestaArrays.php
@@ -0,0 +1,20 @@
+<?php
+
+/*$cursos = array("CUR003" => "Python","Ruby");
+
+$cursos["CUR012"] = "Logica"; //OU array_push()
+$cursos[] = "HTML";
+
+for($i = 0; $i< count($cursos); i++)
+{
+  echo "Curso: $cursos[$i]" . PHP_EOL;
+}
+
+foreach ($cursos as $x => $valor) {
+  echo "Chave: $x, Valor: $valor" . PHP_EOL;
+}
+*/
+
+$cursos = array();
+
+$cursos = array(10,20,30);
